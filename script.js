bbb36c6b0b3127acb49d17e10398808a7b05ea63
Gabriela Melnik carvalho <61423263+Gabrielamcarvalho@users.noreply.github.com>
2020-08-20 17:04:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 
 <<<<<<< HEAD
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+
 let ready = false;
 let imagesLoaded = 0;
 let totalImages = 0;
